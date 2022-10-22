920c93c007484d8d21ba083ae77c8cfe83a61577
Marco Antônio <107323705+marcoantoniobjj@users.noreply.github.com>
2022-10-22 17:02:54 -0300

    Add files via upload

--- MP.ApiDotNet6/MP.ApiDotNet6.Application/DTOs/Validations/UserDTOValidator.cs
+++ MP.ApiDotNet6/MP.ApiDotNet6.Application/DTOs/Validations/UserDTOValidator.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using FluentValidation;
+
 namespace MP.ApiDotNet6.Application.DTOs.Validations
 {
-    public class UserDTOValidator : AbstractionValidator<UserDTO>
+    public class UserDTOValidator : AbstractValidator<UserDTO>
     {
         public UserDTOValidator()
         {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Email deve ser informado!");
+
+            RuleFor(x => x.Password)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Password deve ser informado!");
+
+
         }
     }
 }
---
+++ MP.ApiDotNet6/MP.ApiDotNet6.Application/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MP.ApiDotNet6.Application.DTOs;
+using MP.ApiDotNet6.Application.DTOs.Validations;
+using MP.ApiDotNet6.Application.Services.Interfaces;
+using MP.ApiDotNet6.Domain.Authentication;
+using MP.ApiDotNet6.Domain.Repositories;
+
+namespace MP.ApiDotNet6.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenGenerator _tokenGenerator;
+
+        public UserService(IUserRepository userRepository, ITokenGenerator tokenGenerator)
+        {
+            _userRepository = userRepository;
+            _tokenGenerator = tokenGenerator;
+        }
+
+        public async Task<ResultService<dynamic>> GenerateTokenAsync(UserDTO userDTO)
+        {
+            if (userDTO == null)
+                return ResultService.Fail<dynamic>("Objeto deve ser informado");
+
+            var validator = new UserDTOValidator().Validate(userDTO);
+            if (!validator.IsValid)
+                return ResultService.RequestError<dynamic>("Problemas com a validacao", validator);
+
+            var user = await _userRepository.GetUserByEmailAndPassordAsync(userDTO.Email, userDTO.Password);
+            if (user == null)
+                return ResultService.Fail<dynamic>("Usuario ou senha nao encontrado");
+
+            return ResultService.Ok(_tokenGenerator.Generator(user));
+        }
+    }
+}
+
